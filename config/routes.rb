d6d297db0bf55d8f227eed38856a14badf50436b
abo-elleef <a.aboelllef@gmail.com>
2020-07-28 20:02:59 +0300

    limit user resources and remove ui endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
+  # mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  resources :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :users, only: [:create, :update, :show]
   resources :locations
-  resources :clients
   resources :closing_shifts
   resources :shifts
   resources :staffs
+  resources :clients
   get "export/clients", to: "export#clients"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
