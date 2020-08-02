5071202b597001c5f8e8dacaee547bab5c38ed8f
abo-elleef <a.aboelllef@gmail.com>
2020-08-02 10:44:49 +0300

    try [19]

--- spec/requests/clients_specs.rb
+++ spec/requests/clients_specs.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
-RSpec.describe "Clients API", :type => :request do
-
+RSpec.describe "Clients API", type: :request do
   it "list all clients data" do
     get "/clients"
     expect(response.code).to eq("200")
