d2752c7f7b9f989f2e9c1755fbcfb25d07b2d3b2
Mourinto <monier@mailbox.org>
2020-08-04 14:03:52 +0200

    add service category default data to bootstrap

--- app/models/service_category.rb
+++ app/models/service_category.rb
@@ -1,3 +1,11 @@
 class ServiceCategory < ApplicationRecord
   has_many :services
+
+  def self.default_data
+        {
+            name: "Hair ",
+            description: "wonder fancy Hair care",
+            appointment_color: "#00c9e1"
+        }
+  end
 end
