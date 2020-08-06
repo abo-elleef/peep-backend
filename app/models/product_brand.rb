946adb5c86fcce72bb56ab27f29c35cd6a8a77c9
samah elkeneb <samahelkeneb@192.168.1.2>
2020-08-06 11:38:45 +0200

    edit search in products brands

--- app/models/product_brand.rb
+++ app/models/product_brand.rb
@@ -1,4 +1,4 @@
 class ProductBrand < ApplicationRecord
   validates_presence_of :name
-  scope :search, -> (search) { search.present? ?  where("name ilike ?", search) : all }
+  scope :search, -> (search) { search.present? ?  where("name ilike ?", "%" + search + "%") : all }
 end
