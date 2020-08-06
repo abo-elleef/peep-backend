b919a89993f4c77813a8e8836de616fbf6dddaba
Mourinto <monier@mailbox.org>
2020-08-07 01:16:08 +0200

    add comments to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,11 +1,23 @@
 class Client < ApplicationRecord
+  # == Constants ============================================================
+  # == Extensions ===========================================================
+
   enum notify_method: { email: 1 }
 
+  # == Relationships ========================================================
   belongs_to :location, optional: true
 
+  # == Validations ==========================================================
   validates_presence_of :first_name
 
+  # == Scopes ===============================================================
   scope :search, -> (search) { search.present? ?  where("first_name ilike ?", search).
     or(where("last_name ilike ?", search)).
     or(where(phone: search)) : all }
+
+  # == Callbacks ============================================================
+
+  # == Class Methods ========================================================
+
+  # == Instance Methods =====================================================
 end
