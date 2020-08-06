c61b6104ebb323eee2fc24b25010ee7917546788
Mourinto <monier@mailbox.org>
2020-08-06 13:22:30 +0200

    unify the model organizing method

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,10 @@
 class Client < ApplicationRecord
-  validates_presence_of :first_name
-  belongs_to :location, optional: true
   enum notify_method: { email: 1 }
+
+  belongs_to :location, optional: true
+
+  validates_presence_of :first_name
+
   scope :search, -> (search) { search.present? ?  where("first_name ilike ?", search).
     or(where("last_name ilike ?", search)).
     or(where(phone: search)) : all }
