f977e5f26ba680743f666204dd9f3727d8dc2c87
abo-elleef <a.aboelllef@gmail.com>
2020-08-06 23:58:57 +0300

    let appointment endpoint accept filters

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,7 +2,7 @@ class AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:show, :edit, :update, :destroy]
 
   def index
-    appointments = Appointment.all
+    appointments = Appointment.filter(params.slice(:start_at, :end_at, :staff_ids, :location_id))
     render json: AppointmentSerializer.new(appointments), status: :ok
   end
 
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,28 @@
 class Appointment < ApplicationRecord
+  # == Constants ============================================================
+  # == Extensions ===========================================================
+  include Filterable
+
+  # == Relationships ========================================================
   has_many :appointments_services
   has_many :lines, dependent: :destroy
   has_many :services, through: :lines
   has_many :staffs, through: :lines
+
+
+  # == Validations ==========================================================
+  validates_presence_of :location_id
+
+  # == Scopes ===============================================================
+  scope :by_date, -> (start_time, end_time) { where("date => ? AND date <= ?  ", start_time, end_time) }
+  scope :by_location, -> (location_ids) { where(location_id: location_ids) }
+  scope :by_staff, -> (staff_ids){ joins(:lines).where(lines: { staff_id: staff_ids })}
+  scope :by_service, -> (service_ids){ joins(:lines).where(lines: {service_id: service_ids} )}
+
+  # == Callbacks ============================================================
+  #
+  # == Class Methods ========================================================
+  #
+  # == Instance Methods =====================================================
+
 end
