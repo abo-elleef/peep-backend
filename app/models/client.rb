b25c76eb6e5f57bb9f53b7c9dec31f996b1b4f28
Mourinto <monier@mailbox.org>
2020-08-07 15:04:49 +0200

    edit searching scopes method

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@ class Client < ApplicationRecord
   validates_presence_of :first_name
   belongs_to :location, optional: true
   enum notify_method: { email: 1 }
-  scope :search, -> (search) { search.present? ?  where("first_name ilike ?", search).
-    or(where("last_name ilike ?", search)).
+  scope :search, -> (search) { search.present? ?  where("first_name ilike ?", "%" + search + "%").
+    or(where("last_name ilike ?", "%" + search + "%")).
     or(where(phone: search)) : all }
 end
--- app/models/service_category.rb
+++ app/models/service_category.rb
@@ -2,9 +2,10 @@ class ServiceCategory < ApplicationRecord
   include Filterable
   has_many :services
 
-  scope :filter_by_name, -> (name) { where("name ilike ?", name)}
-  scope :filter_by_search, ->(search) {where("name ilike ?", search).
-      or(where("description ilike ?", search)). or(where("appointment_color ilike ?", search)). or all}
+  scope :filter_by_name, -> (name) { where("name ilike ?", "%" + name + "%")}
+  scope :filter_by_search, ->(search) {where("name ilike ?", "%" + search + "%").
+      or(where("description ilike ?", "%" + search + "%")).
+      or(where("appointment_color ilike ?", "%" + search + "%")). or all}
 
   def self.default_data
     [
