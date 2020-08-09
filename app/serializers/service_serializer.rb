60d7afd18478342f3070b8c642d577d2327b22c2
Mourinto <monier@mailbox.org>
2020-08-09 14:37:38 +0200

    define service price serializer name

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -2,5 +2,5 @@ class ServiceSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :description,
              :available_for, :staff_commission, :extra_time, :extra_time_type, :extra_time_duration, :service_category_id
-  has_many :service_prices
+  has_many :service_prices, serializer: ServicePriceSerializer
 end
