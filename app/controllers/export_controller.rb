ac747f25fae1de121491b50f53e59b5095017a22
Mourinto <monier@mailbox.org>
2020-08-12 14:25:37 +0200

    edit constant name

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -1,9 +1,9 @@
 class ExportController < ApplicationController
 
-  EXPORT_OPTIONS = %w(csv xls)
+  CLIENTS_OPTIONS = %w(csv xls)
 
   def clients
-    if params[:export_type].in? EXPORT_OPTIONS
+    if params[:export_type].in? CLIENTS_OPTIONS
       send_data ExportClients.new(params).call, filename: "#{__method__}-#{Date.today}.#{params[:export_type]}"
     else
       render json: {}, status: :unprocessable_entity
