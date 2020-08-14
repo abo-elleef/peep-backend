2954389616c12a0a3531c077dd45d406c4eca09e
abo-elleef <a.aboelllef@gmail.com>
2020-08-14 23:00:17 +0300

    modify serialization for shift

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -11,6 +11,7 @@ class Shift < ApplicationRecord
   belongs_to :location
 
   # == Validations ==========================================================
+  validate :single_day_shift, :valid_times
   # == Scopes ===============================================================
 
   scope :by_location_id, -> (location_id) {where(location_id: location_id)}
@@ -30,4 +31,16 @@ class Shift < ApplicationRecord
   end
 
   # == Instance Methods =====================================================
+
+
+  private
+  def single_day_shift
+    return if start_time.to_date == end_time.to_date
+    errors[:start_time] << "start time and end time should be in the same day"
+  end
+
+  def valid_times
+    return if end_time > start_time
+    errors[:end_time] << "end time must be bigger than stat time "
+  end
 end
--- app/serializers/shift_serializer.rb
+++ app/serializers/shift_serializer.rb
@@ -1,6 +1,10 @@
 class ShiftSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :start_time, :end_time
+
+  attribute :day do |object|
+    object.start_time.wday
+  end
   attribute :staff_name do |object|
     object.staff.name
   end
