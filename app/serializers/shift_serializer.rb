b51505867851085a610373cb3369b7592262f802
abo-elleef <a.aboelllef@gmail.com>
2020-08-15 22:11:04 +0300

    add validation

--- app/serializers/shift_serializer.rb
+++ app/serializers/shift_serializer.rb
@@ -1,6 +1,6 @@
 class ShiftSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :start_time, :end_time, :staff_id
+  attributes :id, :start_time, :end_time, :staff_id, :location_id
 
   attribute :day do |object|
     object.start_time.wday
