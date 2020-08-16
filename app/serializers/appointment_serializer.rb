d333fb370548db42b6d6f68dddc34d7ab1de96d6
Mourinto <monier@mailbox.org>
2020-08-16 20:13:40 +0200

    revert incorrect changes

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,7 +1,7 @@
 class AppointmentSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :status, :client_id, :location_id, :notes, :date,
-             :created_at, :cancellation_reason_id
+             :created_at, :cancellation_id
   has_many :lines
   has_many :payments
 end
