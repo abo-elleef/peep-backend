9b2cc7f05af64d911e13bb516ce95851dadcf7b0
abo-elleef <a.aboelllef@gmail.com>
2020-08-17 21:52:47 +0300

    product retouches

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,8 @@ class Product < ApplicationRecord
   validates_uniqueness_of :name, :barcode
 
   scope :by_search, -> (search) { where("name ilike ?", "%" + search + "%").
-      or(where("barcode ilike ?", search)).
-      or(where("sku ilike ?", search)) }
+      or(where("barcode ilike ?",  "%" + search + "%")).
+      or(where("sku ilike ?",  "%" + search + "%")) }
   scope :by_product_category_id, -> (product_category_id) { where(product_category_id: product_category_id) }
   scope :by_product_brand_id, -> (product_brand_id) { where(product_brand_id: product_brand_id) }
 end
