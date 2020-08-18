5ad5eb3353c4f2e66ae9bec5a4aa4daad1a90602
Mourinto <monier@mailbox.org>
2020-08-18 14:58:54 +0200

    add service price association with line

--- app/serializers/line_serializer.rb
+++ app/serializers/line_serializer.rb
@@ -1,5 +1,5 @@
 class LineSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :appointment_id, :staff_id, :service_id, :price_name, :price, :original_price, :staff_name, :staff_name,
-             :starts_at, :ends_at
+  attributes :appointment_id, :staff_id, :service_id, :service_price_id,
+             :price_name, :price, :original_price, :staff_name, :staff_name, :starts_at, :ends_at
 end
