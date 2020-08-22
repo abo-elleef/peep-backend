c49e2bf2a2923e217253f44be9aabcf4df602dd2
abo-elleef <a.aboelllef@gmail.com>
2020-08-23 02:01:52 +0300

    fix serializing array

--- app/controllers/service_categories_controller.rb
+++ app/controllers/service_categories_controller.rb
@@ -2,7 +2,8 @@ class ServiceCategoriesController < ApplicationController
 
   def index
     service_categories = ServiceCategory.preload(services: [:service_prices]).peep_filter(params.slice(:name, :search)).limit(10)
-    render json: {data: service_categories}, each_serializer: ServiceCategorySerializer,  status: :ok
+    serializers = ActiveModel::Serializer::ArraySerializer.new(service_categories, each_serializer: ServiceCategorySerializer)
+    render json: {data: serializers},  status: :ok
   end
 
   def show
