785a4ccab0ff5393c56a7aca4208b6ead311660c
abo-elleef <a.aboelllef@gmail.com>
2020-08-23 02:57:57 +0300

    fix failing tests

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
-  attributes :first_name, :last_name, :email, :password_digest, :company_name,
+  attributes :id, :first_name, :last_name, :email, :password_digest, :company_name,
              :business_type, :created_at, :updated_at
   has_many :locations, serializer: LocationSerializer
 
-  attribute :token do |user|
-    JwtService.encode(user)
+  attribute :token do |serializer|
+    JwtService.encode(serializer.object)
   end
 end
