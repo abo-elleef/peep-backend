e07886d3dcaacc79b159ed0bf5e3ae736ad5af23
ahmed <a.aboelllef@gmail.com>
2020-08-23 12:28:47 +0300

    serializer fixes (#70)

--- app/serializers/product_category_serializer.rb
+++ app/serializers/product_category_serializer.rb
@@ -2,7 +2,7 @@ class ProductCategorySerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
   attributes :id, :name, :updated_at
   
-  attribute :products_count do |object|
-    object.object.products.size
+  attribute :products_count do |serializer|
+    serializer.object.products.size
   end
 end
--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -2,8 +2,9 @@ class ServiceSerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
   attributes :id, :name, :description, :available_for, :staff_commission,
              :extra_time, :extra_time_type, :extra_time_duration, :service_category_id
-  attribute :category_name do |object|
-    object.object.service_category&.name
+
+  attribute :category_name do |serializer|
+    serializer.object.service_category&.name
   end
 
   has_many :service_prices, serializer: ServicePriceSerializer
