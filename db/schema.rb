a90d0d033e1601eea9292ca528007e5c44fbde4e
ahmed <a.aboelllef@gmail.com>
2020-08-26 03:30:38 +0300

    retouches (#73)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_174227) do
+ActiveRecord::Schema.define(version: 2020_08_26_002554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -294,7 +294,6 @@ ActiveRecord::Schema.define(version: 2020_08_25_174227) do
     t.integer "duration"
     t.integer "pricing_type"
     t.float "price", default: 0.0
-    t.float "special_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["service_id"], name: "index_service_prices_on_service_id"
