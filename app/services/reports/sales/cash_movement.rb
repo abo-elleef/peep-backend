6505147d5f1ebf7ae3e043fdfeee0d7e3f1b876a
abo-elleef <a.aboelllef@gmail.com>
2020-09-07 06:11:37 +0300

    modifications to daily sales

--- app/services/reports/sales/cash_movement.rb
+++ app/services/reports/sales/cash_movement.rb
@@ -5,27 +5,28 @@ module Reports
       attr_reader :starts_at, :ends_at, :location_id
 
       def initialize(params)
-        @starts_at = params[:starts_at]
-        @ends_at = params[:ends_at]
+        start_date = params[:starts_at] || Time.zone.now.to_s
+        end_date = params[:ends_at] || Time.zone.now.to_s
+        @starts_at = Time.zone.parse(start_date).beginning_of_day
+        @ends_at = Time.zone.parse(end_date).end_of_day
         @location_id = params[:location_id].presence
       end
 
       def perform
-        {
-            cash: build_cash_data
-        }
+        [build_payment_type, build_vouchers].flatten
       end
-
       private
-      #TODO add missing data
-      def build_cash_data
-        {
-            total: "",
-            refunds: ""
-        }
+
+      def build_payment_type
+        PaymentType.joins(:payments).where(payments: {created_at: starts_at..ends_at}).
+            group("payment_types.name").
+            select("payment_types.name, Sum(payments.amount)").all.map(&:attributes)
       end
 
-
+      def build_vouchers
+        sum = 123 # @monier build the right query
+        {name: "Voucher Redemptions", sum: sum}
+      end
     end
   end
 end
--- app/services/reports/sales/transaction_summary.rb
+++ app/services/reports/sales/transaction_summary.rb
@@ -5,31 +5,49 @@ module Reports
       attr_reader :starts_at, :ends_at, :location_id
 
       def initialize(params)
-        @starts_at = params[:starts_at]
-        @ends_at = params[:ends_at]
+        start_date = params[:starts_at] || Time.zone.now.to_s
+        end_date = params[:ends_at] || Time.zone.now.to_s
+        @starts_at = Time.zone.parse(start_date).beginning_of_day
+        @ends_at = Time.zone.parse(end_date).end_of_day
         @location_id = params[:location_id].presence
       end
 
       def perform
-        {
-            services: build_services
-        }
+        [
+            build_services,
+            build_products,
+            build_vouchers
+        ]
       end
 
       private
 
       def build_services
-        lines = if location_id.present?
-                  Line.includes(:appointment).where(appointments: {location_id: location_id}).where(created_at: DateTime.parse(starts_at)..DateTime.parse(ends_at)).pluck("count(lines.id), SUM(lines.price)").flatten
-                else
-                  Line.where(created_at: DateTime.parse(starts_at)..DateTime.parse(ends_at))
-                      .pluck("count(id), SUM(price)").flatten
+        {name: :services}.merge!(build_lines("ServicePrice"))
+      end
 
-                end
+      def build_products
+        {name: :products}.merge!(build_lines("Product"))
+      end
+
+      def build_vouchers
+        # TODO @monier build the right query
+        {name: :vouchers}.merge!({
+            quantity: 23,
+            total_price: 123345
+        })
+      end
+
+
+      def build_lines(type)
+        lines = Line.where(created_at: starts_at..ends_at).where(sellable_type: type)
+        if location_id.present?
+          lines = lines.includes(:appointment).where(appointments: {location_id: location_id})
+        end
+        lines = lines.pluck("count(lines.id), SUM(lines.price)").flatten
         {
-            quantity: lines.first,
-            refunds: "",
-            total_price: lines.last
+            quantity: lines.first.to_f,
+            total_price: lines.last.to_f
         }
       end
 
