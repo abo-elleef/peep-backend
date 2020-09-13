57c2f1493cab2fb505403cf4a11198b1d070f72d
abo-elleef <a.aboelllef@gmail.com>
2020-09-13 08:53:50 +0200

    retouches for invoices

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,10 +1,17 @@
 class InvoicesController < ApplicationController
 
   def index
-    appointment_ids =
-        params[:appointment_id].present? ? params[:appointment_id] : Invoice.joins(:appointment).where(appointments: {location_id: params[:location_id]}).pluck(:appointment_id)
-    invoices = InvoiceBuilder.new(appointment_ids).call
-    render json: {invoices: invoices}, status: :ok
+    invoices = Invoice.joins(:appointment).where(appointments: {location_id: params[:location_id]})
+    pagy, invoices = pagy(invoices, page: page_index, items: page_size)
+    serializers = ActiveModel::Serializer::ArraySerializer.new(invoices, serializer: InvoiceSerializer)
+    render json: {data: serializers, meta: pagy_meta_data(pagy)},  status: :ok
+
+  end
+
+  def show
+    appointment_id = params[:appointment_id]
+    invoices = InvoiceBuilder.new([appointment_id]).call
+    render json: {data: invoices}, status: :ok
   end
 end
 
