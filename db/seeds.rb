4851ee105bb8b9f784ff605ab1870ad8052a6623
abo-elleef <a.aboelllef@gmail.com>
2020-09-19 09:33:18 +0200

    comment seed.rb out

--- db/seeds.rb
+++ db/seeds.rb
@@ -185,43 +185,43 @@
 #                       })
 # end
 # ========== create Appointments with lines  =======================================================
-start_date = Date.new(2020, 8, 29);
-hours = (6..18).to_a;
-days = [];
-staffs = Staff.all;
-clients = Client.all;
-locations = Location.all;
-prices = ServicePrice.all
-21.times {|index| days << start_date + index.day };
-1000.times do |appointment_index|
-  location = locations.sample
-  staff = staffs.sample
-  client = clients.sample
-  service_price = prices.sample
-  day = days.sample
-  time = day + hours.sample.hour
-  appointment_services = []
-  rand(1..3).times do |index|
-    start_time = time + rand(1..3).hour
-    end_time = start_time + rand(60..120).minute
-    appointment_services << {
-        staff_id: staff.id,
-        starts_at: start_time,
-        ends_at: end_time,
-        service_id: service_price.service.id,
-        service_price_id: service_price.id,
-        created_at: time,
-        updated_at: time
-    }
-  end
-  Appointment.create!({
-                          status: rand(1...6),
-                          client_id: client.id,
-                          location_id: location.id,
-                          notes: " #{appointment_index}-- long appointment notes",
-                          date: day,
-                          created_at: time,
-                          updated_at: time,
-                          appointment_services_attributes: appointment_services
-                      })
-end
+# start_date = Date.new(2020, 8, 29);
+# hours = (6..18).to_a;
+# days = [];
+# staffs = Staff.all;
+# clients = Client.all;
+# locations = Location.all;
+# prices = ServicePrice.all
+# 21.times {|index| days << start_date + index.day };
+# 1000.times do |appointment_index|
+#   location = locations.sample
+#   staff = staffs.sample
+#   client = clients.sample
+#   service_price = prices.sample
+#   day = days.sample
+#   time = day + hours.sample.hour
+#   appointment_services = []
+#   rand(1..3).times do |index|
+#     start_time = time + rand(1..3).hour
+#     end_time = start_time + rand(60..120).minute
+#     appointment_services << {
+#         staff_id: staff.id,
+#         starts_at: start_time,
+#         ends_at: end_time,
+#         service_id: service_price.service.id,
+#         service_price_id: service_price.id,
+#         created_at: time,
+#         updated_at: time
+#     }
+#   end
+#   Appointment.create!({
+#                           status: rand(1...6),
+#                           client_id: client.id,
+#                           location_id: location.id,
+#                           notes: " #{appointment_index}-- long appointment notes",
+#                           date: day,
+#                           created_at: time,
+#                           updated_at: time,
+#                           appointment_services_attributes: appointment_services
+#                       })
+# end
