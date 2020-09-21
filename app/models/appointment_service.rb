8434f2d81655d3e7099ce6821c76200c382c945f
Monier <monier@mailbox.org>
2020-09-21 05:14:47 +0200

    edit staff status between checking

--- app/models/appointment_service.rb
+++ app/models/appointment_service.rb
@@ -1,10 +1,20 @@
 class AppointmentService < ApplicationRecord
+  # == Constants ============================================================
+  # == Extensions ===========================================================
   include Filterable
+  # == Attributes ===========================================================
+  # == Relationships ========================================================
   belongs_to :appointment
   belongs_to :service_price
   belongs_to :staff
 
+  # == Validations ==========================================================
+  # == Scopes ===============================================================
+  scope :overlaps?, -> (starts_at, ends_at) { where("created_at <= ? AND ? <= created_at", ends_at, starts_at).any? }
 
+  # == Callbacks ============================================================
+  # == Class Methods ========================================================
+  # == Instance Methods =====================================================
   def service
     service_price&.service
   end
