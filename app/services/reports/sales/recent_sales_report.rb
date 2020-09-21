789d5d0e2a65d0ede57989847984e5d0c908be92
Monier <monier@mailbox.org>
2020-09-21 14:43:35 +0200

    add recent sales

--- app/services/reports/sales/recent_sales_report.rb
+++ app/services/reports/sales/recent_sales_report.rb
@@ -1,30 +1,31 @@
 module Reports
   module Sales
     class RecentSalesReport
-      attr_reader :range, :location_id
+      attr_reader  :location_ids, :to_date
 
       def initialize(params)
-        @range = params[:range]
-        @location_id = params[:location_id].presence
+        @location_ids = params[:location_id].present? ? params[:location_id] : Location.all.pluck(:id).join(',')
+        @to_date = Date.today - params[:range].to_i.days
       end
 
       def perform
-        appointments_count =
-            if range == 7
-              Appointment.by_location_ids(location_id).joins(appointment_services: :service_price).where("appointments.created_at >= ? AND appointments.created_at <= ?", (Date.today - 7.days), Date.today).where(status: 5).select("count(*) as app_count, SUM(service_prices.price) as values")
-            else
-              Appointment.by_location_ids(location_id).joins(appointment_services: :service_price).where("appointments.created_at >= ? AND appointments.created_at <= ?", (Date.today - 30.days), Date.today).where(status: 5).select("count(*) as app_count, SUM(service_prices.price) as values")
-            end
-
+        appointments_count = Appointment.by_location_ids(location_ids).where("created_at >= ? AND created_at <= ?", to_date, Date.today ).where(status: 5).count
+        sales = (to_date..Date.today).map do |day|
+          Line.includes(:invoice).where("invoices.location_id IN (?)", location_ids).where("lines.created_at >= ? AND lines.created_at <= ?", day.beginning_of_day, day.end_of_day).sum('lines.unit_price*lines.quantity')
+        end
+        sales_values =  sales.sum
+        appointments = (to_date..Date.today).map do |day|
+          Appointment.by_location_ids(location_ids).joins(appointment_services: :service_price).where("appointments.created_at >= ? AND appointments.created_at <= ?", day.beginning_of_day, day.end_of_day).where(status: 5).sum("service_prices.price")
+        end
+        appointment_values = appointments.sum
         {
-            appointment_count: 23,
-            appointment_value: 1023,
-            sales_value: 256,
-            sales: [23, 45, 76, 12, 12, 78, 90],
-            appointments: [23, 45, 76, 12, 12, 78, 90],
-            days: (Date.today..(Date.today + 7.days)).map { |a| a.strftime("%Y-%m-%d") }
+            appointment_count: appointments_count,
+            appointment_value: appointment_values,
+            sales_value: sales_values,
+            sales: sales,
+            appointments: appointments,
+            days: (to_date..Date.today).map { |a| a.strftime("%Y-%m-%d") }
         }
-
       end
     end
   end
