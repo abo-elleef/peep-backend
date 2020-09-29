0990cd4307b4d74f6f41a0d5dffd2d00bf1fdc45
Monier <monier@mailbox.org>
2020-09-29 19:42:39 +0200

    edit user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,9 +2,10 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    username { Faker::Internet.username }
     email { Faker::Internet.email }
-    password_digest { Faker::Internet.password }
     company_name { Faker::Company.name }
+    password { Faker::Internet.password }
   end
 end
 
