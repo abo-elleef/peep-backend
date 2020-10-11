383a71600c1dd130b46852f2d5991d0627f5ad59
ahmed <a.aboelllef@gmail.com>
2020-10-11 19:03:25 +0300

    add retouches to products to accept stock control for different locations (#117)
    Co-authored-by: aboelleef <ahmedaboeleef@trycarriage.com>

--- app/serializers/locations_product_serializer.rb
+++ app/serializers/locations_product_serializer.rb
@@ -2,6 +2,7 @@ class LocationsProductSerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
   attributes :id, :product_id, :location_id, :initial_stock, :reorder_point,
              :reorder_quantity, :created_at, :updated_at, :location_name
+
   def location_name
     object.location.name
   end
