7d0aa5d0b57fecea3c7787fec6db5b33dfb74102
aboelleef <ahmedaboeleef@trycarriage.com>
2020-10-16 19:53:33 +0200

    client profile modifications

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,6 +1,11 @@
 class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :status, :client_id, :location_id, :notes, :date,
-             :created_at, :cancellation_reason_id, :starts_at, :ends_at
+             :created_at, :cancellation_reason_id, :starts_at, :ends_at, :total
+
+  def total
+    object.service_prices.map(&:price).compact.sum
+  end
+
   has_many :appointment_services
   has_many :service_prices
 end
