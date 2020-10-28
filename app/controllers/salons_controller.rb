5c69fb3d82daabc2049cea119a2431b5ec465ecd
Monier <monier@mailbox.org>
2020-10-28 12:18:34 +0200

    add services staffs

--- app/controllers/salons_controller.rb
+++ app/controllers/salons_controller.rb
@@ -11,8 +11,26 @@ class SalonsController < ApplicationController
   end
 
   def location_staffs
-    binding.pry
     location = Location.find(params[:location_id])
-    @staffs = location.staffs
+    @services_prices = ServicePrice.includes(service: :staffs).where(service_prices: {id: params["prices"]})
+    @staffs = @services_prices.map do |service_price|
+      {
+          staff_id: service_price.service.staffs.first.id,
+          staff_name: service_price.service.staffs.first.name,
+          service_name: service_price.service.name,
+          service_duration: service_price.duration,
+          service_price: service_price.price,
+          service_id: service_price.service.id
+      }
+    end
   end
+
+  def confirm_selection
+    
   end
+
+  def service_staffs
+    service = Service.find params[service_id]
+    service.staffs
+  end
+end
