d220653c0aaab585853083ab2ef54e0d3f4aa585
aboelleef <ahmedaboeleef@trycarriage.com>
2020-11-01 08:55:48 +0200

    fix quantity calculation for service prices

--- app/services/checkout/invoice_calculation.rb
+++ app/services/checkout/invoice_calculation.rb
@@ -2,7 +2,7 @@ module Checkout
   class InvoiceCalculation
 
     def self.perform(params)
-      total = (params[:lines_attributes].presence&.values || []).sum { |line| line[:unit_price].to_f * line[:quantity].to_f }
+      total = (params[:lines_attributes].presence&.values || []).sum { |line| line[:unit_price].to_f * [line[:quantity].to_f, 1].max }
       tips_total = (params[:tips_attributes].presence&.values || []).sum { |tip| tip[:value].to_f }
       paid = (params[:payments_attributes].presence&.values || []).sum { |payment| payment[:amount].to_f }
       balance = (total + tips_total) - paid
