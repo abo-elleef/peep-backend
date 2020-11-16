65441de32fbc443517d9141d6e92ffdabfe38f12
aboelleef <ahmedaboeleef@trycarriage.com>
2020-11-16 07:04:36 +0200

    new edit form styling for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -19,6 +19,15 @@ class LocationsController < ApplicationController
     render json: {data: LocationSerializer.new(location)}, status: :ok
   end
 
+
+  def edit
+    @location = Location.find params[:id]
+  end
+
+  def new
+    @location = Location.new
+  end
+
   def create
     location = Location.new(location_params)
     if location.save
